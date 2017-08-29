135ef1656efe304fb84a328deb42369a3bccd7df
Drew Lemmy <drew@lemmmy.pw>
2017-08-29 01:33:54 +0100

    message descriptions

--- src/rocketchat/attachments/file.attachment.js
+++ src/rocketchat/attachments/file.attachment.js
@@ -3,13 +3,17 @@ let webserverURL;
 function fileAttachmentHandler(rc, msg) {
   let id = msg.file._id;
   let name = encodeURIComponent(msg.file.name);
+  let description = msg.file.description;
 
   let token = rc.connection.webserverToken;
 
   let url = `${webserverURL}/file/${id}/${name}?t=${token}`;
 
-  msg.msg += "\n";
-  msg.msg += url;
+  msg.msg += "\n" + url;
+
+  if (description) {
+    msg.msg += "\n" + description;
+  }
 }
 
 module.exports = (rocketchat) => {
